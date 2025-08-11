17401f79430f4acb0466da0a03eed2d78d3edfe5
Vinicius Sales de Souza <viniciussds4@gmail.com>
2025-08-11 19:56:36 -0300

    Atualização do Deploy - Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,22 +7,23 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-TypeOrmModule.forRootAsync({
-	useClass: ProdService,
-    imports: [ConfigModule],
-}),
-   PostagemModule,
-   TemaModule,
-   AuthModule,
-   UsuarioModule
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+    }),
+    PostagemModule,
+    TemaModule,
+    AuthModule,
+    UsuarioModule
   ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
 
 
