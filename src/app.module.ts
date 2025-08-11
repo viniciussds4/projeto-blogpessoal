e729515c5c31f6e9f769e8f6f49ff62c4eca1bed
Vinicius Sales de Souza <viniciussds4@gmail.com>
2025-08-11 18:30:20 -0300

    Deploy do projeto Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostagemModule } from './postagem/postagem.module';
-import { Postagem } from './postagem/entities/postagem.entity';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost', 
-      port: 3306,
-      username: 'root',  
-      password: 'vaLon@SZ32_$', //senha SQL
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
-      logging: true,
-
-    }),
+    ConfigModule.forRoot(),
+TypeOrmModule.forRootAsync({
+	useClass: ProdService,
+    imports: [ConfigModule],
+}),
    PostagemModule,
    TemaModule,
    AuthModule,
